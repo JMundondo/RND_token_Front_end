cff9b18c7e44dc61892b2225b774a6d6cd9299d6
J_Mundondo <jephiastmundondo@gmail.com>
2022-11-01 17:43:14 +0200

    tailwind and react working

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bond py-12 text-red-700 underline">
+    Hello world tailwind! lets go.
+    
+  </h1>
+  );
+}
+
+export default App;
