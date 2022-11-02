41cd1e7ff4c270c5280d2a1e864a301358732933
J_Mundondo <jephiastmundondo@gmail.com>
2022-11-02 10:36:41 +0200

    dizzle did not initiaze

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import RoundToken from './artifacts/RoundToken.json' ;
+import RoundTokenCrowedSale from './artifacts/RoundTokenCrowedSale.json';
+import {DizzleProvider} from '@drizzle/react-plugin' ; 
+import {LoadingContainer} from '@drizzle/react-components' ;
+
+
+
+const drizzleOptions = {
+  contracts: [
+   RoundToken ,
+   RoundTokenCrowedSale
+  ], 
+}
 
 function App() {
   return (
-    <h1 className="text-3xl font-bond py-12 text-red-700 underline">
-    Hello world tailwind! lets go.
+    <DizzleProvider options={drizzleOptions}>
+      <LoadingContainer>
+        <div>
+          <h1 className="text-3xl font-bond py-12 text-red-700 underline">
+          Hello world tailwind! lets go.
+          </h1>
+        </div>
+      </LoadingContainer>
+    </DizzleProvider>
+
     
-  </h1>
+  
   );
 }
 
