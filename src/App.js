745de21cbd853b0b5c0c8969adbeddc5f958ffed
J_Mundondo <jephiastmundondo@gmail.com>
2022-11-03 06:15:34 +0200

    dizzle working

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@
 import './App.css';
 import RoundToken from './artifacts/RoundToken.json' ;
 import RoundTokenCrowedSale from './artifacts/RoundTokenCrowedSale.json';
-import {DizzleProvider} from '@drizzle/react-plugin' ; 
-import {LoadingContainer} from '@drizzle/react-components' ;
+import { DrizzleContext } from '@drizzle/react-plugin';
+import { Drizzle } from "@drizzle/store";
 
 
 
@@ -13,18 +13,35 @@ const drizzleOptions = {
    RoundTokenCrowedSale
   ], 
 }
+const drizzle = new Drizzle(drizzleOptions);
+
 
 function App() {
   return (
-    <DizzleProvider options={drizzleOptions}>
-      <LoadingContainer>
-        <div>
+    <DrizzleContext.Provider drizzle={drizzle}>
+   
+    <DrizzleContext.Consumer>
+    {drizzleContext => {
+      const {initialized} = drizzleContext;
+
+      if(!initialized) {
+        return "Loading..."
+      }
+
+      return (
+         
+          <div>
           <h1 className="text-3xl font-bond py-12 text-red-700 underline">
-          Hello world tailwind! lets go.
+            Hello world tailwind! lets go.
           </h1>
         </div>
-      </LoadingContainer>
-    </DizzleProvider>
+        )
+      }}
+  </DrizzleContext.Consumer>
+
+</DrizzleContext.Provider>
+
+   
 
     
   
